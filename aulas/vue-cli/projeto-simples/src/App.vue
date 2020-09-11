8810f4fb850952a549619ccd4282cd885b51be7f
emersonBarreiro <32658839+emersonBarreiro@users.noreply.github.com>
2020-09-11 17:14:43 -0300

    Estudos dia 2020-09-11
    Arquivos referentes às aulas dos módulos 5 e 6

---
+++ aulas/vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">{{titulo}}<button @click="titulo += '#'">Alterar Título</button></div>
+  
+</template>
+
+<script>
+
+//este é equivalente à instância vue
+//sendo que podemos usar data e outros aqui
+export default {
+  data: function(){
+      return{
+        titulo: 'Teste Data usando vue js'
+      }
+  },
+  
+}
+</script>
+
+<style>
+  #app{
+    background-color: chocolate;
+    color: #fff;
+  }
+</style>
+
